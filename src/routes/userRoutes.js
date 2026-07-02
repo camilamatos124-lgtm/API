fff126bbc9d9d0ef52d44a270fb1af23abc4a18c
camilamatos124-lgtm <camila.matos.124@ufrn.edu.br>
2026-07-02 07:26:00 -0300

    Ajeitando o swagger

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,75 @@
 import { Router } from "express";
-import UserController from "../controllers/userController.js";
+import userController from "../controllers/userController.js";
 
-const usersRouter = Router();
+const router = Router();
 
-// Rota aberta para criar conta
-usersRouter.post("/register", UserController.register);
+/**
+ * @swagger
+ * tags:
+ *   name: Usuários
+ *   description: Sistema de Autenticação e Criptografia (Aulas 18 e 21)
+ */
 
-// Rota aberta para fazer login
-usersRouter.post("/login", UserController.login);
+/**
+ * @swagger
+ * /api/usuarios/register:
+ *   post:
+ *     summary: Registra um novo usuário na nuvem
+ *     tags: [Usuários]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - nome
+ *               - email
+ *               - password
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *                 default: user
+ *     responses:
+ *       201:
+ *         description: Usuário criado com sucesso (Senha criptografada com Bcrypt!)
+ *       409:
+ *         description: Este e-mail já está em uso.
+ */
+router.post("/register", userController.register);
 
-export default usersRouter;
+/**
+ * @swagger
+ * /api/usuarios/login:
+ *   post:
+ *     summary: Realiza o login do usuário e gera o Token JWT
+ *     tags: [Usuários]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login realizado com sucesso! Retorna o Token JWT.
+ *       401:
+ *         description: Credenciais inválidas.
+ */
+router.post("/login", userController.login);
+
+export default router;
